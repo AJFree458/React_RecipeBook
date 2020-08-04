c9c5b9f6f89777dc46dbd2406e73f362b2668b1e
Arthur Freeman <Arthur@AJ-Freeman-MacBook.local>
2020-08-04 16:07:26 -0400

    Correct margin class for buttons

--- client/src/components/Banner/LandingBanner.js
+++ client/src/components/Banner/LandingBanner.js
@@ -9,10 +9,10 @@ function LandingBanner() {
         <h1 className="display-4">Welcome to Your RecipeBook!</h1>
         <h2>Save and view your recipes here.</h2>
         <hr className="my-4" />
-        <Button color="primary m-1" href="/register" role="button">
+        <Button color="primary" className="m-1" href="/register" role="button">
           Register
         </Button>
-        <Button color="primary m-1" href="/login" role="button">
+        <Button color="primary" className="m-1" href="/login" role="button">
           Login
         </Button>
       </Jumbotron>
