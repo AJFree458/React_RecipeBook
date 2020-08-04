6980652f40f96282cc286be7beec7efc6be03a9e
Arthur Freeman <Arthur@AJ-Freeman-MacBook.local>
2020-08-04 15:10:19 -0400

    Add main banner component

--- client/src/components/Banner/index.js
+++ client/src/components/Banner/index.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import { Jumbotron, Button } from 'reactstrap';
+import LandingBanner from '../Banner/LandingBanner';
+import MemberBanner from '../Banner/MemberBanner';
+import AuthService from '../../services/AuthService';
 import './style.css';
 
+var IsAuthenticated = AuthService.GetAuthStatus();
+
 function Banner() {
-  return (
-    <div>
-      <Jumbotron className="bannerimage">
-        <h1 className="display-4">Welcome to Your RecipeBook!</h1>
-        <h2>Save and view your recipes here.</h2>
-        <hr className="my-4" />
-        <Button color="primary" href="/register" role="button">
-          Register
-        </Button>
-        <Button color="primary" href="/login" role="button">
-          Login
-        </Button>
-      </Jumbotron>
-    </div>
-  );
+  if (IsAuthenticated) {
+    return <MemberBanner />;
+  }
+
+  return <LandingBanner />;
 }
 
 export default Banner;
