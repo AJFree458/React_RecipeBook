53ab3b23c849224eca3a6328bb7c3082950b35d5
Arthur Freeman <Arthur@AJ-Freeman-MacBook.local>
2020-08-05 13:47:40 -0400

    Server.js change for production

--- server.js
+++ server.js
@@ -22,11 +22,12 @@ app.use(routes);
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-  // Define any API routes before this runs
-  app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, './client/build/index.html'));
-  });
 }
+
+// Define any API routes before this runs
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/recipebook', {
