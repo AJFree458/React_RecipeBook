f53248d2d5a79867cc09d6b9c712cc56396e8952
Arthur Freeman <Arthur@AJ-Freeman-MacBook.local>
2020-08-05 10:56:30 -0400

    Remove an array from the model

--- models/Recipe.js
+++ models/Recipe.js
@@ -12,41 +12,34 @@ const recipeSchema = new Schema({
     type: String,
     required: true,
   },
+  ingredients: [
+    {
+      qty: {
+        type: Number,
+      },
+      unit: {
+        type: String,
+      },
+      ingredient: {
+        type: String,
+      },
+    },
+  ],
   prepTime: {
     type: String,
   },
   cookTime: {
     type: String,
   },
-  ingredients: [
-    {
-      quantity: {
-        type: Number,
-      },
-      unit: {
-        type: String,
-      },
-      ingredientName: {
-        type: String,
-      },
-    },
-  ],
   servings: {
     type: String,
   },
   equipment: {
     type: String,
   },
-  directions: [
-    {
-      step: {
-        type: Number,
-      },
-      instruction: {
-        type: String,
-      },
-    },
-  ],
+  directions: {
+    type: String,
+  },
 });
 
 const Recipe = mongoose.model('recipe', recipeSchema);
